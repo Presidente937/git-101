bd21af87b169c04fef8a416563618928b89ee55d
Mattia De Rinaldis <mattia.derinaldis@gmail.com>
2019-03-04 12:40:48 +0100

    add approximate_size description

--- humansize.py
+++ humansize.py
@@ -1,6 +1,9 @@
 SUFFIXES = ['KB', 'MB', 'GB', 'TB', 'PB', 'EX', 'ZB', 'YB']
 
 def approximate_size(size):
+    """convert a file size to human/readable form
+    """
+    
     multiple=1024
     for suffix in SUFFIXES:
         size/=multiple
